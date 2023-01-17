e06a35085b8c70337e26f79328628612cc35dfd0
gana1009 <107204258+gana1009@users.noreply.github.com>
2023-01-17 23:29:07 +0530

    Time: 17 ms (97.72%), Space: 11.1 MB (52.66%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int countFlip =0;
+        int countOne =0;
+        for(auto i : s)
+        {
+            if(i == '1') 
+            {
+                countOne ++;
+            }
+            else
+            {
+                countFlip = min(++countFlip , countOne);
+            }
+        }
+        return countFlip;
+    }
+};
